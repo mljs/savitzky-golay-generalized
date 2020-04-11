d76f3aac9c62e9a09640058638bf2d4fccc6bbdf
Timothy Vogel <vogel.timothya@gmail.com>
2020-04-11 19:23:11 +1200

    test: add border-values test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -81,4 +81,21 @@ describe('Savitzky–Golay', () => {
       expect(ans[j]).toBeCloseTo(ans2[j], 10);
     }
   });
+
+  it('Border test', () => {
+    let options = {
+      windowSize: 9,
+      derivative: 1,
+      polynomial: 3,
+    };
+
+    let data = new Array(20);
+    for (let i = 0; i < data.length; i++) {
+      data[i] = Math.pow(i, 3) - 4 * Math.pow(i, 2) + 5 * i;
+    }
+    let ans = SG(data, 1, options);
+    for (let j = 0; j < data.length; j++) {
+      expect(ans[j]).toBeCloseTo(3 * Math.pow(j, 2) - 8 * j + 5, 6);
+    }
+  });
 });
